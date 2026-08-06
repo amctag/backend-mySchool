c4b139f80534d44fcab79ce3a8dbeae7e68e2fdf
Your Name <your_email@example.com>
2026-08-06 12:54:24 +0300

    seed1

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'ts-node src/database/seed/seed.ts',
+    seed: 'node dist/database/seed/seed.js',
   },
   datasource: {
     url:
